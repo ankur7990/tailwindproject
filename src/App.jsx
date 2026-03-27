cd8700f505e1c19a9ae71f089122bd5a0ee842ce
Ankur <ankur.teksun@gmail.com>
2026-03-27 12:33:31 +0530

    Started Project. First comit.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Login from "./Login";
+import Signup from "./Signup";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="Enter Email" />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
