55bf06ae5f05d15ff2e6c1f7e4ecc507adbbbf8c
hitkacha <hitkacha15@gmail.com>
2026-04-27 17:33:34 +0530

    button textfield and gradient css added .

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,8 +3,32 @@ import React from "react";
 function Login() {
   return (
     <div>
-      <div>
-        <input type="text" placeholder="Enter Email" />
+      <div className="flex justify-content items-center gap-4">
+        <label className="  gap-4 m-2 p-2">
+          Name
+          <input
+            type="text"
+            placeholder="Enter Email"
+            className="bg-gray-100 border-solid border-2 border-indigo-600 rounded-2xl h-10 "
+          />
+        </label>
+        <label className="    gap-4 m-2">
+          Password
+          <input
+            type="text"
+            placeholder="Enter Email"
+            className="bg-gray-100 border-solid border-2 border-indigo-600 rounded-2xl h-10 "
+          />
+        </label>
+        <label className="    gap-4 m-2">
+          retype Password
+          <input
+            type="text"
+            placeholder="Enter Email"
+            className="bg-gray-100 border-solid border-2 border-indigo-600 rounded-2xl h-10 "
+          />
+        </label>
+        <button>Save</button>
       </div>
     </div>
   );
